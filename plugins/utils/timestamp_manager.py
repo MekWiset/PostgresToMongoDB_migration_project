6bb3260505b52b4a9ef4b79ef7e90325c9598fc9
Siriwat Wisetpakdeewong <139029208+swisetpakdeewong@users.noreply.github.com>
2024-07-23 11:56:15 +0700

    add timestamp_manager util plugin

---
+++ plugins/utils/timestamp_manager.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from datetime import datetime
+
+class timestampManager:
+
+    '''
+    Manage timestamp files for data processing.
+    '''
+
+    def __init__(self, timestamp_path):
+        self.timestamp_path = timestamp_path
+
+    def read_timestamp(self) -> datetime:
+        '''Read and return the timestamp from the file.'''
+        try:
+            with open(self.timestamp_path, 'r') as file:
+                timestamp_str = file.read().strip()
+
+                if timestamp_str:
+                    return datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+                else:
+                    print('Timestamp file is empty')
+                    return None
+
+        except FileNotFoundError:
+            print('Timestamp file cannot be found')
+            return None
+
+    def compare_timestamp(self, lastexe_df: pd.DataFrame, lastexe_col: str) -> pd.DataFrame:
+        '''Return DataFrame with data newer than the stored timestamp.'''
+        timestamp = self.read_timestamp()
+
+        if timestamp is not None:
+            lastexe_df[lastexe_col] = pd.to_datetime(lastexe_df[lastexe_col])
+            newly_generated_data = lastexe_df[lastexe_df[lastexe_col] > timestamp]
+        else:
+            newly_generated_data = lastexe_df.copy()
+
+        return newly_generated_data
+
+    def update_timestamp(self) -> None:
+        '''Update the timestamp file with the current time.'''
+        with open(self.timestamp_path, 'w') as file:
+            file.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
